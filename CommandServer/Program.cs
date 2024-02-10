5c7ddd449f931a10cb506dab996c938988328f41
weitsunglin <wilson.lwt.tw@gmail.com>
2024-02-11 00:22:05 +0800

    multichat first commit

---
+++ CommandServer/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json; //NuGet 管理器安装
+using DBManager;
+
+namespace CommandServer
+{
+    class Program
+    {
+        private const int serverPort = 6000; // 服务器端口号
+        private static TcpListener listener;
+        private static NetworkStream stream;
+        private static byte[] dataBytes = new byte[4096];
+
+        static void Main(string[] args)
+        {
+            StartServer();
+            while (true){
+                Console.WriteLine("等待客戶端連線");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("客戶端已連線");
+                HandleClient(client);
+                //client.Close();
+                //Console.WriteLine("客戶端斷線");
+            }
+        }
+
+        private static void StartServer(){
+            try{
+                // 创建 TCP 服务器监听器
+                listener = new TcpListener(IPAddress.Any, serverPort);
+                // 启动服务器
+                listener.Start();
+                Console.WriteLine("command Server started. Waiting for connections...");
+            }
+            catch (Exception ex){
+                Console.WriteLine("Failed to start command server: " + ex.Message);
+            }
+        }
+
+        public class CommandData
+        {
+            public string Command { get; set; }
+            public string Account { get; set; }
+            public string Password { get; set; }
+        }
+
+       
+
+        private static void HandleClient(TcpClient client)
+        {
+            Console.WriteLine("1111111111111111111111 ") ;
+            try
+            {
+                Console.WriteLine("222222222222222222222 ");
+                stream = client.GetStream();
+
+                
+                Console.WriteLine("222222222222222222222 ");
+                Array.Clear(dataBytes, 0, dataBytes.Length);
+
+
+                //if (stream.DataAvailable)
+                //{
+                    int bytesRead = stream.Read(dataBytes, 0, dataBytes.Length);
+                    // 处理读取的数据
+                    Console.WriteLine("222222222222222222222 ");
+                    // 将接收到的字节数组转换为字符串
+                    string jsonData = Encoding.ASCII.GetString(dataBytes, 0, bytesRead);
+                    Console.WriteLine("222222222222222222222 ");
+ 
+                //}
+
+
+                // 将字符串解析为数据对象
+                CommandData data = JsonConvert.DeserializeObject<CommandData>(jsonData);
+
+                Console.WriteLine("Received command: " + data.Command);
+
+
+                string user_name = "";
+
+                // 执行命令
+                if (data.Command == "COMMAND1") {
+                    Console.WriteLine("3333333333333333333333 ");
+                    string response;
+                    // TODO: 执行相应的操作
+                    if (DBClass.CheckAccount(data.Account, data.Password)==true) {
+                        Console.WriteLine("444444444444444444444444444 ");
+                        // Console.WriteLine("yyyyyyyyy: " + data.Account);
+
+                        user_name = DBClass.GetUsdrName(data.Account);
+
+                        //Console.WriteLine("xxxxxxx: " + user_name);
+
+                        response = "true"+","+ user_name;
+                    }
+                    else {
+                        Console.WriteLine("5555555555555555555555555555 ");
+                        response = "false" + "," + user_name;
+                    }
+                    Console.WriteLine("6666666666666666666666666666 ");
+                    byte[] responseData = Encoding.ASCII.GetBytes(response);
+
+                    //Console.WriteLine("將回應資料寫入網路流: " + response);
+                    // 將回應資料寫入網路流
+                    stream.Write(responseData, 0, responseData.Length);
+                    stream.Flush();
+                }
+                
+            }
+            catch (Exception ex){
+                Console.WriteLine("Error handling client: " + ex.Message);
+            }
+        }
+
+
+        private static void StopServer(){
+            listener.Stop();
+            Console.WriteLine("Server stopped.");
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using DBManager;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MainServer
+{
+    class Program{
+        public static int PROTOCOL = 5000;
+        static void Main(string[] args){
+
+            InitTcpListener( PROTOCOL );
+        }
+
+
+
+        private static string username = "";
+
+        private static TcpListener tcpListener;
+
+        private static List<TcpClient> Clients = new List<TcpClient>();
+
+        private static  Dictionary<TcpClient, string> hashMap = new Dictionary<TcpClient, string>();
+
+        //private static int mapIndex = 0;
+
+        public static void InitTcpListener(int PROTOCOL)
+        {
+            tcpListener = new TcpListener(IPAddress.Any, PROTOCOL);
+            tcpListener.Start();
+            Console.WriteLine("Main server已啟動");
+            while (true)
+            {
+
+
+                TcpClient client = tcpListener.AcceptTcpClient();
+
+
+
+                if (client != null)
+                {
+                    StreamReader reader = new StreamReader(client.GetStream());
+                    username = reader.ReadLine();
+                    /*mapIndex = mapIndex + 1;*/
+                    hashMap.Add(client, username);
+                    Console.WriteLine("新的用戶已連線:" + username);
+                    Clients.Add(client);
+                    Thread thread = new Thread(ClientListener);
+                    thread.Start(client);
+                    continue;
+                }
+            }
+
+        }
+
+        public static void ClientListener(object clientObj)
+        {
+            TcpClient client = (TcpClient)clientObj;
+
+            try
+            {
+               
+                StreamReader reader = new StreamReader(client.GetStream());
+                while (true)
+                {
+                    string message = reader.ReadLine();
+                    BroadCast(message, client);
+                    Console.WriteLine(message, "客戶端發送的新訊息");
+                }
+            }
+            catch (IOException ex)
+            {
+               
+                Console.WriteLine("客戶端以斷線: " + ex.Message);
+
+                DBClass.UpdateUserLoginState(hashMap[client], 0);
+                return;
+            }
+        }
+
+
+        public static void BroadCast(string msg, TcpClient excludeClient)
+        {
+            foreach (TcpClient client in Clients)
+            {
+                if (client.Connected && client != excludeClient)
+                {
+                    StreamWriter sWriter = new StreamWriter(client.GetStream());
+                    sWriter.WriteLine(msg);
+                    sWriter.Flush();
+                    Console.WriteLine("成功廣播訊息給所有客戶端");
+                }
+            }
+        }
+    }
+}
